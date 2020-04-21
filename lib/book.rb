a523ce57449c0542343ff1ced39e199a58e6ede3
David Munro <dbhmunro@gmail.com>
2020-04-21 23:59:41 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,7 +3,7 @@ class Book
     @title = title
   end
 
-  attr_accessor :title :author :page_count :genres
+  attr_accessor :title, :author, :page_count, :genres
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
